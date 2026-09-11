ffdcdd570eb0f019407f5f3f6e3f36d07d70b5f9
psrathore88 <130173982+psrathore88@users.noreply.github.com>
2026-09-11 13:32:08 +0530

    Update footer.jsx

--- src/sections/footer.jsx
+++ src/sections/footer.jsx
@@ -10,7 +10,7 @@ export function Footer() {
     <div className="max-w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
       <div className="xl:grid xl:grid-cols-3 xl:gap-16">
         <div className="space-y-8 xl:col-span-1">
-          <span className="text-2xl font-bold">INTERNATIONAL CONFERENCE ON SMART AND DISTRIBUTED COMPUTING 2025</span>
+          <span className="text-2xl font-bold">INTERNATIONAL CONFERENCE ON SMART AND DISTRIBUTED COMPUTING 2027</span>
           {/* <p className="text-gray-400 text-base">
             For queries, contact: <a href="mailto:hod.cce@jaipur.manipal.edu" className="underline">hod.cce@jaipur.manipal.edu</a>
           </p> */}
